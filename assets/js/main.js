a84c05f2bcacbccbe9ccc577f89ecb90b01e03e3
klimas7 <klimas7@gmail.com>
2020-11-10 23:26:13 +0100

    #97 Menu #6

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,13 @@ $(window).resize(sectionHeight);
 
 $(function() {
     $("section h1, section h2").each(function(){
-        $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'>" +
+        let h = this.nodeName.toLowerCase();
+        console.log("h: " + h);
+        $("nav ul").append("<li class='tag-" + h + "'>" +
             "<a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a>" +
             "</li>");
+
+
         $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
         $("nav ul li:first-child a").parent().addClass("active");
     });
